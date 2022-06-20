3db52d5e49c052cfdd11a39fc3e0007f54ef7e14
Rango Yuan <yuanoook@gmail.com>
2022-06-20 16:53:50 +0000

    update test

--- test/guesswhat-test.js
+++ test/guesswhat-test.js
@@ -10,7 +10,7 @@ describe("GuessWhat", function () {
     [contract, , addr1, addr2, addr3] = await prepare("GuessWhat", N`10`);
   });
 
-  it("Should update defender with a new challenge without defender", async function () {
+  it("Should update defender with a new challenge if there's no defender", async function () {
     expect(await contract.defender()).to.equal(ethers.constants.AddressZero);
     expect(await contract.challenger()).to.equal(ethers.constants.AddressZero);
 
@@ -20,7 +20,7 @@ describe("GuessWhat", function () {
     expect(await contract.challenger()).to.equal(ethers.constants.AddressZero);
   });
 
-  it("Should update challenger with a new challenge with a defender", async function () {
+  it("Should update challenger with a new challenge if there is defender", async function () {
     expect(await contract.defender()).to.equal(ethers.constants.AddressZero);
     expect(await contract.challenger()).to.equal(ethers.constants.AddressZero);
 
@@ -44,5 +44,37 @@ describe("GuessWhat", function () {
     await expect(
       contract.connect(addr3).challenge(HashZero)
     ).to.be.revertedWith("GuessWhat: move not allowed");
+  });
+
+  it("Should be able to defend with a challenge in effect", async function () {
+    expect(await contract.defender()).to.equal(ethers.constants.AddressZero);
+    expect(await contract.challenger()).to.equal(ethers.constants.AddressZero);
+
+    await tx(contract.connect(addr1).challenge(HashZero));
+    await tx(contract.connect(addr2).challenge(HashZero));
+
+    expect(await contract.defender()).to.equal(addr1.address);
+    expect(await contract.challenger()).to.equal(addr2.address);
+
+    await expect(contract.connect(addr1).defend(HashZero)).not.to.be.reverted;
+  });
+
+  it("Should not be able to defend without a challenge in effect", async function () {
+    expect(await contract.defender()).to.equal(ethers.constants.AddressZero);
+    expect(await contract.challenger()).to.equal(ethers.constants.AddressZero);
+
+    await expect(contract.connect(addr1).defend(HashZero)).to.be.revertedWith(
+      "GuessWhat: move not allowed"
+    );
+
+    await tx(contract.connect(addr1).challenge(HashZero));
+    await expect(contract.connect(addr1).defend(HashZero)).to.be.revertedWith(
+      "GuessWhat: move not allowed"
+    );
+
+    await tx(contract.connect(addr2).challenge(HashZero));
+    await expect(contract.connect(addr3).defend(HashZero)).to.be.revertedWith(
+      "GuessWhat: you are not allowed"
+    );
   });
 });
