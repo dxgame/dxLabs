a798d58e1763a0a25e76c4c80e47cd2533671804
Rango Yuan <yuanoook@gmail.com>
2022-06-21 12:12:32 +0000

    Update GuessWhat

--- contracts/GuessWhat.sol
+++ contracts/GuessWhat.sol
@@ -11,14 +11,14 @@ function isOne(string memory str) pure returns (bool) {
     return bytes(str)[0] == 0x31;
 }
 
-function hashHex(string memory str) pure returns (string) {
+function hashHex(string memory str) pure returns (string memory) {
     return Strings.toHexString(uint(keccak256(abi.encodePacked(str))));
 }
 
-function whoWins(Game _game) pure returns (address) {
-    string memory revealedRequest = _game.states[2].message;
-    string memory revealedResponse = _game.states[3].message;
-    return isOne(revealedResponse) == isOne(revealedRequest) ? _game.defender() : _game.challenger();
+function whoWins(Game storage game) pure returns (address) {
+    string memory revealedRequest = game.states[2].message;
+    string memory revealedResponse = game.states[3].message;
+    return isOne(revealedResponse) == isOne(revealedRequest) ? game.defender() : game.challenger();
 }
 
 contract GuessWhat is Ownable, ERC20 {
@@ -51,46 +51,51 @@ contract GuessWhat is Ownable, ERC20 {
     }
 
     function challenge(
-        bytes32 prehash, address _player, string memory encryptedRequest, uint8 v, bytes32 r, bytes32 s
+        bytes32 prehash, address player, string memory encryptedRequest, uint8 v, bytes32 r, bytes32 s
     ) private nextMoveIs(Step.ONE_ChallengeStarted) {
         game.start(
-            State(prehash, _player, encryptedRequest, v, r, s),
+            State(prehash, player, encryptedRequest, v, r, s),
             game.winner
         );
     }
 
     function defend(
-        bytes32 prehash, address _player, string memory encryptedResponse, uint8 v, bytes32 r, bytes32 s
+        bytes32 prehash, address player, string memory encryptedResponse, uint8 v, bytes32 r, bytes32 s
     ) external nextMoveIs(Step.TWO_DefenderDefended) {
         game.play(
-            State(prehash, _player, encryptedResponse, v, r, s),
+            State(prehash, player, encryptedResponse, v, r, s),
             whoWins
         );
     }
 
     function challengerReveal(
-        bytes32 prehash, address _player, string memory revealedRequest, uint8 v, bytes32 r, bytes32 s
+        bytes32 prehash, address player, string memory revealedRequest, uint8 v, bytes32 r, bytes32 s
     ) external nextMoveIs(Step.THREE_ChallengerRevealed) {
         require(game.states[0].message == hashHex(revealedRequest), "GuessWhat: do not match");
 
         game.play(
-            State(prehash, _player, revealedRequest, v, r, s),
+            State(prehash, player, revealedRequest, v, r, s),
             whoWins
         );
     }
 
     function defenderReveal(
-        bytes32 prehash, address _player, string memory revealedResponse, uint8 v, bytes32 r, bytes32 s
+        bytes32 prehash, address player, string memory revealedResponse, uint8 v, bytes32 r, bytes32 s
     ) external nextMoveIs(Step.FOUR_DefenderRevealed) {
         require(game.states[1].message == hashHex(revealedResponse), "GuessWhat: do not match");
 
         game.play(
-            State(prehash, _player, revealedResponse, v, r, s),
+            State(prehash, player, revealedResponse, v, r, s),
             whoWins
         );
     }
 
-    function claimWinning() external {
-
+    function claimWinning(
+        bytes32 prehash, address player, string memory message, uint8 v, bytes32 r, bytes32 s
+    ) external {
+        game.claimWinning(
+            State(prehash, player, message, v, r, s),
+            whoWins
+        );
     }
 }
