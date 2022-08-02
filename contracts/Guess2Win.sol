2261f54c5b16d7867930453c138211cd6097ceff
Doge Ragnar <yuanoook@gmail.com>
2022-08-02 09:45:50 +0000

    update

--- contracts/Guess2Win.sol
+++ contracts/Guess2Win.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.14;
 
 contract Guess2Win {
-    uint256 constant public min_freeze_time = 1 hours;
     uint256 constant public max_reveal_time = 1 days;
+    uint256 constant public min_freeze_time = 1 hours;
 
     struct Game {
         address token;
@@ -21,6 +21,7 @@ contract Guess2Win {
         uint256 revealTime;
         uint256 freezeTime;
         uint256 expiryTime;
+
         bool delisted;
         bool claimed;
     }
@@ -31,7 +32,10 @@ contract Guess2Win {
         uint256 indexed id,
         address indexed owner,
         uint256 question,
-        uint256 startAt
+        uint256 startAt,
+        uint256 revealTime,
+        uint256 freezeTime,
+        uint256 expiryTime
     );
 
     event Reply(
@@ -50,10 +54,46 @@ contract Guess2Win {
         address indexed winner
     );
 
-    constructor() public {}
+    function add (
+        address token,
+        uint256 amount,
+        bytes32 question,
 
-    function add () {
-        transfer();
+        uint256 revealTime,
+        uint256 freezeTime,
+        uint256 expiryTime
+    ) public returns (uint256 id) {
+        require(amount > 0, "You need to put at least some tokens");
+        require(revealTime <= max_reveal_time, "Reveal time must be at most 1 day");
+        require(freezeTime >= min_freeze_time, "Freeze time must be at least 1 hour");
+        require(expiryTime >= min_freeze_time, "Expiry time must be at least 1 hour");
+
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Check the token allowance");
+        token.transferFrom(msg.sender, address(this), amount);
+
+        Game game = Game({
+            token: token,
+            amount: amount,
+            owner: msg.sender,
+            question: question,
+            startAt: block.timestamp,
+
+            revealTime: revealTime,
+            freezeTime: freezeTime,
+            expiryTime: expiryTime
+        });
+
+        games.push(game);
+        Add(games.length - 1,
+            game.owner,
+            game.question,
+            game.startAt,
+            game.revealTime,
+            game.freezeTime,
+            game.expiryTime
+        );
+        return games.length - 1;
     }
 
     function delist(uint256 id) external {
